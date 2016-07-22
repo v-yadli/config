9450b07e3d6d18d36a4429311a0979834708c78a
Yatao Li <yatao.li@live.com>
2016-07-22 22:30:24 +0800

    ...

--- bootstrap.bat
+++ bootstrap.bat
@@ -7,19 +7,22 @@ choco install -y ag
 choco install -y 7zip
 choco install -y autohotkey
 choco install -y autoit
-choco install -y sumatrapdf
+choco install -y sumatrapdf.install
 choco install -y tortoisehg
 choco install -y everything
 choco install -y firefox
-choco install -y altdrag
 choco install -y adblockplusie
+choco install -y flashplayerplugin
+choco install -y altdrag
 choco install -y dia
 choco install -y emacs
+choco install -y k-litecodecpackmega
 choco install -y gimp
 choco install -y inkscape
 choco install -y visualstudio2013ultimate --execution-timeout 300000
 choco install -y visualstudio2015enterprise --execution-timeout 300000
 choco install -y visualstudio2013-sdk
+choco install -y nuget.commandline
 choco install -y notepadplusplus
 choco install -y sysinternals
 choco install -y cmder
@@ -31,6 +34,11 @@ choco install -y curl
 choco install -y wget
 choco install -y windbg
 choco install -y f.lux
+choco install -y jdk8
+choco install -y linqpad
+choco install -y racket
+choco install -y sbcl
+choco install -y androidstudio
 REM choco install -y startkiller
 
 refreshenv
