d5a414a120bd3fe75de265b8a467e833df91b1c4
Yatao Li <yatli@microsoft.com>
2018-02-11 21:10:24 +0800

    update ps profile, add mintty color config for colortool

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,63 @@
-Import-Module posh-git
+﻿new-alias -name "vim" -value nvim
+new-alias -name "vi" -value nvim
+new-alias -name "gvim" -value nvim-qt
+
+$gitLoaded = $false
+function Import-Git($Loaded){
+    if($Loaded) { return }
+    $GitModule = Get-Module -Name Posh-Git -ListAvailable
+    if($GitModule | select version | where version -le ([version]"0.6.1.20160330")){
+        Import-Module Posh-Git > $null
+    }
+    if(-not ($GitModule) ) {
+        Write-Warning "Missing git support, install posh-git with 'Install-Module posh-git' and restart cmder."
+    }
+    # Make sure we only run once by alawys returning true
+    return $true
+}
+
+function checkGit($Path) {
+    if (Test-Path -Path (Join-Path $Path '.git') ) {
+        $gitLoaded = Import-Git $gitLoaded
+        Write-VcsStatus
+        return
+    }
+    $SplitPath = split-path $path
+    if ($SplitPath) {
+        checkGit($SplitPath)
+    }
+}
+
+if (Get-Module PSReadline -ErrorAction "SilentlyContinue") {
+    Set-PSReadlineOption -ExtraPromptLineCount 1
+}
+
+<#
+This scriptblock runs every time the prompt is returned.
+Explicitly use functions from MS namespace to protect from being overridden in the user session.
+Custom prompt functions are loaded in as constants to get the same behaviour
+#>
+[ScriptBlock]$Prompt = {
+    $realLASTEXITCODE = $LASTEXITCODE
+    $host.UI.RawUI.WindowTitle = Microsoft.PowerShell.Management\Split-Path $pwd.ProviderPath -Leaf
+    $Host.UI.RawUI.ForegroundColor = "White"
+    Microsoft.PowerShell.Utility\Write-Host $pwd.ProviderPath -NoNewLine -ForegroundColor Green
+    Microsoft.PowerShell.Utility\Write-Host " :: [ $([System.DateTime]::Now) ]" -NoNewLine -ForegroundColor Yellow
+    checkGit($pwd.ProviderPath)
+    Microsoft.PowerShell.Utility\Write-Host "`nλ " -NoNewLine -ForegroundColor "DarkGray"
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return " "
+}
+
+# Functions can be made constant only at creation time
+# ReadOnly at least requires `-force` to be overwritten
+Set-Item -Path function:\prompt  -Value $Prompt  # -Options ReadOnly
+
 Import-Module ZLocation
-Import-Module PSColor
-Import-Module Watch-PerfCounter
-Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadlineKeyHandler Ctrl+Backspace -Function BackwardKillWord
-Set-PSReadlineKeyHandler Ctrl+LeftArrow -Function BackwardWord
-Set-PSReadlineKeyHandler Ctrl+RightArrow -Function NextWord
+Set-PSReadlineKeyHandler -Key Ctrl+w -Function BackwardKillWord
+Set-PSReadlineKeyHandler -Key Alt+w -Function NextWord
+Set-PSReadlineKeyHandler -Key Alt+b -Function BackwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+a -Function BeginningOfLine
+Set-PSReadlineKeyHandler -Key Ctrl+e -Function EndOfLine
+
