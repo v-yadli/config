082ec97e1468b41d79548853c869c8c332887fe4
Yatao Li <yatli@microsoft.com>
2018-04-05 23:36:19 +0800

    ...

--- setup_paths.ps1
+++ setup_paths.ps1
@@ -1,11 +1,13 @@
 $env = Get-ItemProperty -Path HKCU:\Environment
 $p = $env.Path
 
+#todo find out where is anaconda
+
 $newPaths = (
+        #"C:\tools\Anaconda3\Scripts",
+        #"C:\tools\Anaconda3",
         "C:\Program Files\CMake\bin",
-        "C:\tools\neovim\Neovim\bin",
-        "C:\tools\Anaconda3\Scripts",
-        "C:\tools\Anaconda3"
+        "C:\tools\neovim\Neovim\bin"
         )
 
 if(!$p.EndsWith(";")) { $p = "$p;" }
@@ -26,7 +28,7 @@ Function Set-EnvironmentVariable([string] $key, [string] $val)
 }
 
 Set-EnvironmentVariable -key Path -val $p
-Set-EnvironmentVariable -key PYTHON_INCLUDE -val "C:\tools\Anaconda3\include"
-Set-EnvironmentVariable -key PYTHON_LIB -val "C:\tools\Anaconda3\include"
+#Set-EnvironmentVariable -key PYTHON_INCLUDE -val "C:\tools\Anaconda3\include"
+#Set-EnvironmentVariable -key PYTHON_LIB -val "C:\tools\Anaconda3\lib"
 # Set-EnvironmentVariable -key HTTP_PROXY -val "127.0.0.1:1080"
 # Set-EnvironmentVariable -key HTTPS_PROXY -val "127.0.0.1:1080"
