acaedee3d9d27791703b8d6d1504aea99d19a6c9
Yatao Li <yatli@microsoft.com>
2018-04-05 23:31:38 +0800

    ...

--- setup_paths.ps1
+++ setup_paths.ps1
@@ -2,7 +2,7 @@ $env = Get-ItemProperty -Path HKCU:\Environment
 $p = $env.Path
 
 $newPaths = (
-        "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin",
+        "C:\Program Files\CMake\bin",
         "C:\tools\neovim\Neovim\bin",
         "C:\tools\Anaconda3\Scripts",
         "C:\tools\Anaconda3"
@@ -28,3 +28,5 @@ Function Set-EnvironmentVariable([string] $key, [string] $val)
 Set-EnvironmentVariable -key Path -val $p
 Set-EnvironmentVariable -key PYTHON_INCLUDE -val "C:\tools\Anaconda3\include"
 Set-EnvironmentVariable -key PYTHON_LIB -val "C:\tools\Anaconda3\include"
+# Set-EnvironmentVariable -key HTTP_PROXY -val "127.0.0.1:1080"
+# Set-EnvironmentVariable -key HTTPS_PROXY -val "127.0.0.1:1080"
