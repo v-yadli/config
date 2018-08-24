fcf473800c55419490ffa703cba5bcae0445512a
Yatao Li <yatli@microsoft.com>
2018-08-24 21:15:23 +0800

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,19 +2,19 @@
 
 THIS_USER=`pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1 | sed 's/[()]//g'`
 
-mkdir bootstrap && cd bootstrap
+mkdir $HOME/bootstrap && cd $HOME/bootstrap
 
-# install latest cmake
+echo "Install cmake..."
 
 wget -q https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.sh
 sh cmake-3.12.0-Linux-x86_64.sh --prefix=/usr/local --exclude-subdir
 
-# install anaconda
+echo "Install anaconda..."
 
 wget -q https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 sudo -u $THIS_USER sh Anaconda3-5.2.0-Linux-x86_64.sh -b -f
 
-# configure .NET installation
+echo "Configure .NET..."
 
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
@@ -22,13 +22,13 @@ dpkg -i packages-microsoft-prod.deb
 apt install -y software-properties-common
 apt install -y apt-transport-https
 
-# configure various ppa
+echo "Configure various ppa..."
 
 apt-add-repository -y ppa:neovim-ppa/unstable
 add-apt-repository -y ppa:timsc/swig-3.0.12
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
-# install packages
+echo "Install apt packages..."
 
 apt update
 apt install -y dotnet-sdk-2.1
@@ -37,6 +37,8 @@ apt install -y neovim
 apt install -y unzip
 apt install -y llvm-6.0-doc llvm-6.0-examples llvm-6.0-tools llvm-6.0-runtime llvm-6.0 llvm-6.0-dev libclang-6.0-dev clang-6.0 clang-tools-6.0 clang-6.0-doc clang-6.0-examples
 apt install -y python-pip
+
+echo "Configuring user profile..."
 
 cat >> $HOME/.bashrc <<EOF
 PATH=\$HOME/anaconda3/bin:\$PATH
