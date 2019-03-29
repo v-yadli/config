8f834d6e8bc82c3251bb703217974c69d8e365a4
Yatao Li <yatli@microsoft.com>
2019-03-29 14:28:53 +0800

    update setup_ps.ps1

--- setup_ps.ps1
+++ setup_ps.ps1
@@ -1,9 +1,10 @@
 Copy-Item -Path "$PSScriptRoot\Microsoft.PowerShell_profile.ps1" -Destination $PROFILE
 Copy-Item -Path "$PSScriptRoot\_vsvimrc" -Destination "$env:USERPROFILE\_vsvimrc"
-Install-Module posh-git -AllowClobber
-Install-Module ZLocation -AllowClobber
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module posh-git -AllowClobber -Force
+Install-Module ZLocation -AllowClobber -Force
 # Install-Module Watch-PerfCounter
 # Install-Module Posh-SSH
-Install-Module InvokeBuild
-Install-Module Pscx -AllowClobber
+Install-Module InvokeBuild -Force
+Install-Module Pscx -AllowClobber -Force
 
